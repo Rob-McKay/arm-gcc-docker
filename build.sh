cc9bc8de420f23c9a0fbe5a5a5f2f3f020ff1a07
Rob McKay <robert.mckay@mining.komatsu>
2021-04-14 17:58:45 +0100

    Updated shell build script

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-docker build -t rmckaykm/gcc-arm-none-eabi-10-2020-q4-major:latest -f src/Dockerfile .
+docker build -t rmckaykm/gcc-10.2.1-arm-none-eabi:gcc-arm-none-eabi-10-2020-q4-major -f src/Dockerfile .
 
-#docker scan  --accept-license rmckaykm/gcc-arm-none-eabi-10-2020-q4-major:latest -f src/Dockerfile
-
-#docker run --rm -it -V $(pwd):/home/build rmckaykm/gcc-arm-none-eabi-10-2020-q4-major:latest apt list --installed
+#docker run --rm -it -V $(pwd):/home/build rmckaykm/gcc-10.2.1-arm-none-eabi:gcc-arm-none-eabi-10-2020-q4-major apt list --installed
