67ef07c4ed2adbe7bded1123dc73a72be5484909
Rob McKay <robert.mckay@mining.komatsu>
2021-11-16 14:30:57 +0000

    Added script for ARM GCC 10.3

---
+++ build-gcc10.3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker build -t rmckaykm/gcc-10.3.1-arm-none-eabi:latest -f src/Dockerfile-gcc-10.3 .
+
+#docker run --rm -it -V $(pwd):/home/build rmckaykm/gcc-10.2.1-arm-none-eabi:gcc-arm-none-eabi-10-2020-q4-major apt list --installed
